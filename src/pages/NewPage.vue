8c2f4f93d7d5a981d3bd3e11d5ac81594d0a5de4
Maggie Cameron <maggie.rose.cameron@gmail.com>
2023-06-05 13:22:03 +1000

    first commit

---
+++ src/pages/NewPage.vue
@@ -0,0 +1,15 @@
+<template>
+    <Layout>
+      <h1>NEW PAGE CREATED BY ME</h1>
+      <p>hello hi i made this</p>
+    </Layout>
+  </template>
+  
+  <script>
+  export default {
+    metaInfo: {
+      title: 'New Page'
+    }
+  }
+  </script>
+  
